75319c4e94888e5a43cce43359a8609233c3745e
Abraão Levi Vitoriano <112588920+Abraao8levi@users.noreply.github.com>
2023-11-03 14:13:38 -0300

    código
    Aqui é um chatbot  simples utilizando a API do GPT

---
+++ Chatbot/chatbot.py
@@ -0,0 +1,22 @@
+import openai
+
+# Substitua 'chave_api' pela sua chave de API real
+chave_api = "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"
+openai.api_key = chave_api
+
+def enviar_mensagem(mensagem):
+    resposta = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "Você está conversando com um assistente virtual."},
+            {"role": "user", "content": mensagem}
+        ],
+    )
+    return resposta["choices"][0]["message"]
+
+while True:
+    texto = input("Escreva sua mensagem: ")
+    if texto.lower() == "sair":
+        break
+    resposta = enviar_mensagem(texto)
+    print(resposta)
